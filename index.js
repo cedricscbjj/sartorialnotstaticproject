78748752cf6016a11483f90a9187e6814b472a32
cedricscbjj <c.saint.charles.csc@gmail.com>
2025-03-22 23:52:49 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// Définir le dossier contenant les fichiers statiques (HTML, CSS, JS)
+app.use(express.static(path.join(__dirname, "public")));
+
+// Route pour servir la page principale
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Démarrer le serveur
+app.listen(PORT, () => {
+    console.log(`Serveur lancé sur http://localhost:${PORT}`);
+});
